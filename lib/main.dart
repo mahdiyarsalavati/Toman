13e70673f94d67c95a9fc6d9828e4cc839f59e5d
Mahdiyar Salavati <mahdiyarsalavati@gmail.com>
2024-07-19 09:08:15 +0330

    updates themeData

--- lib/main.dart
+++ lib/main.dart
@@ -16,50 +16,35 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.white,
         iconTheme: const IconThemeData(color: Colors.white),
         textTheme: const TextTheme(
-          headline1: TextStyle(
+          titleLarge: TextStyle(
               fontFamily: 'IRANSansX',
               fontSize: 24,
               fontWeight: FontWeight.bold,
               color: Colors.white),
-          headline2: TextStyle(
+          headlineMedium: TextStyle(
               fontFamily: 'IRANSansX',
               fontSize: 22,
               fontWeight: FontWeight.bold,
               color: Colors.white),
-          headline3: TextStyle(
+          bodyLarge: TextStyle(
               fontFamily: 'IRANSansX',
               fontSize: 20,
               fontWeight: FontWeight.bold,
               color: Colors.white),
-          headline4: TextStyle(
+          bodyMedium: TextStyle(
               fontFamily: 'IRANSansX',
               fontSize: 18,
               fontWeight: FontWeight.bold,
               color: Colors.white),
-          headline5: TextStyle(
+          bodySmall: TextStyle(
               fontFamily: 'IRANSansX',
               fontSize: 16,
               fontWeight: FontWeight.bold,
               color: Colors.white),
-          headline6: TextStyle(
-              fontFamily: 'IRANSansX',
-              fontSize: 14,
-              fontWeight: FontWeight.bold,
-              color: Colors.white),
-          subtitle1: TextStyle(
-              fontFamily: 'IRANSansX', fontSize: 16, color: Colors.white),
-          subtitle2: TextStyle(
+          labelLarge: TextStyle(
               fontFamily: 'IRANSansX', fontSize: 14, color: Colors.white),
-          bodyText1: TextStyle(
-              fontFamily: 'IRANSansX', fontSize: 14, color: Colors.white),
-          bodyText2: TextStyle(
+          labelSmall: TextStyle(
               fontFamily: 'IRANSansX', fontSize: 12, color: Colors.white),
-          button: TextStyle(
-              fontFamily: 'IRANSansX', fontSize: 14, color: Colors.white),
-          caption: TextStyle(
-              fontFamily: 'IRANSansX', fontSize: 12, color: Colors.white),
-          overline: TextStyle(
-              fontFamily: 'IRANSansX', fontSize: 10, color: Colors.white),
         ),
       ),
       home: CurrencyScreen(),
